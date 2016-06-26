bb808141dc0b259d41a68ff114e844ec268dab11
Alex <alex@krien.name>
2016-06-26 23:43:32 +0200

    Map Graphic

---
+++ src/main/java/de/krien/games/OnlineMonsters/controller/play/Play.java
@@ -0,0 +1,16 @@
+package de.krien.games.OnlineMonsters.controller.play;
+
+import de.krien.games.OnlineMonsters.controller.core.IGameState;
+
+public class Play implements IGameState {
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+}
