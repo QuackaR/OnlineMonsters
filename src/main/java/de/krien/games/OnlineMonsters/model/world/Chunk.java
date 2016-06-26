d18f801824d49f6c2c8746e2af1b5542804f514d
Alex <alex@krien.name>
2016-06-26 23:51:57 +0200

    ...

---
+++ src/main/java/de/krien/games/OnlineMonsters/model/world/Chunk.java
@@ -0,0 +1,16 @@
+package de.krien.games.OnlineMonsters.model.world;
+
+import org.newdawn.slick.opengl.Texture;
+
+public class Chunk {
+
+    private final int TILE_SIZE = 48;
+    private final int TILE_COUNT = 64;
+
+    private Texture chunkImage;
+    private boolean[][] blockedTiles;
+
+    private void draw() {
+        
+    }
+}
---
+++ src/main/java/de/krien/games/OnlineMonsters/model/world/World.java
@@ -0,0 +1,9 @@
+package de.krien.games.OnlineMonsters.model.world;
+
+public class World {
+
+    private Chunk[][] chunks;
+
+
+
+}
